e3e5ee44e04b502fcdeb3336f4a95e862aef8446
BaoNgoc <s3924436@rmit.edu.vn>
2023-08-11 13:22:07 +0700

    Initial Commit

---
+++ PikachuGame/PikachuGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  PikachuGameApp.swift
+//  PikachuGame
+//
+//  Created by Nguyen The Bao Ngoc on 11/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct PikachuGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
